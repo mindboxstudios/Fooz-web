2e2f66fe6c455218cec335e9e65b4a08e7f07d98
Noah Bass <bassjnoah@gmail.com>
2015-08-17 17:38:49 -0400

    Add goalController and goalsController to app.js

--- static/js/app.js
+++ static/js/app.js
@@ -10,6 +10,14 @@ app.config(['$routeProvider', function($routeProvider) {
             templateUrl: 'scoreboard/index.html',
             controller: 'scoreboardController'
         })
+        .when('/goals', {
+            templateUrl: 'goals/index.html',
+            controller: 'goalsController'
+        })
+        .when('/goals/:id/edit', {
+            templateUrl: 'goals/edit.html',
+            controller: 'goalController'
+        })
         .when('/players', {
             templateUrl: 'players/index.html',
             controller: 'playersController'
@@ -18,12 +26,46 @@ app.config(['$routeProvider', function($routeProvider) {
             templateUrl: 'players/edit.html',
             controller: 'playerController'
         })
-        .when('/players/:id', {
+        .when('/players/:id/edit', {
             templateUrl: 'players/edit.html',
             controller: 'playerController'
         });
 }]);
 
+app.service('goalService', ['$http', function ($http) {
+    var urlBase = 'http://localhost:3000/api/v1/goals';
+
+    this.getGoals = function () {
+        return $http.get(urlBase);
+    };
+
+    this.getGoal = function (id) {
+        return $http.get(urlBase + '/' + id);
+    };
+
+    this.saveGoal = function (data) {
+        return $http({
+            url: urlBase,
+            method: 'POST',
+            data: data
+        });
+    };
+
+    this.updateGoal = function (id, data) {
+        return $http({
+            url: urlBase + '/' + id,
+            method: 'PUT',
+            data: data
+        });
+    };
+
+    this.removeGoal = function (id) {
+        return $http({
+            url: urlBase + '/' + id,
+            method: 'DELETE'
+        });
+    };
+}]);
 
 app.service('playerService', ['$http', function ($http) {
     var urlBase = 'http://localhost:3000/api/v1/players';
@@ -60,12 +102,69 @@ app.service('playerService', ['$http', function ($http) {
     };
 }]);
 
+app.controller('goalsController', ['$scope', 'goalService', function($scope, goalService) {
+    goalService.getGoals()
+        .success(function (result) {
+            $scope.goals = result;
+        });
+
+    $scope.remove = function(index) {
+        var goal = $scope.goals[index];
+
+        if(confirm('Are you sure you want this goal scored by ' + goal.for.firstName)) {
+            goalService.removeGoal(goal._id)
+                .success(function (result) {
+                    console.log(result);
+                    $scope.goals.splice(index, 1);
+                });
+        }
+    };
+}]);
+
+app.controller('goalController', ['$scope', '$location', '$routeParams', 'goalService', 'playerService', function($scope, $location, $routeParams, goalService, playerService) {
+    goalService.getGoal($routeParams.id)
+        .success(function (result) {
+            $scope.goal = result;
+        });
+
+    playerService.getPlayers()
+        .success(function (result) {
+            $scope.players = result;
+        });
+
+    $scope.addAgainst = function() {
+        $scope.goal.against.push({});
+    };
+
+    $scope.removeAgainst = function(index) {
+        $scope.goal.against.splice(index, 1);
+    };
+
+    $scope.save = function(goal) {
+        goalService.updateGoal($routeParams.id, goal)
+            .success(function (result) {
+                console.log(result);
+                $location.path('/goals');
+            });
+    };
+}]);
+
 app.controller('playersController', ['$scope', 'playerService', function($scope, playerService) {
     playerService.getPlayers()
         .success(function (result) {
             $scope.players = result;
         });
 
+    // set order to display players,
+    // lastName by default
+    $scope.order = 'lastName';
+
+    // set new order to display players
+    $scope.setOrder = function (order) {
+        $scope.order = order;
+    };
+
+    // Remove a player
     $scope.remove = function(index) {
         var player = $scope.players[index];
 
@@ -79,7 +178,7 @@ app.controller('playersController', ['$scope', 'playerService', function($scope,
     };
 }]);
 
-app.controller('playerController', ['$scope', '$routeParams', '$location', 'playerService', function($scope, $location, $routeParams, playerService) {
+app.controller('playerController', ['$scope', '$location', 'playerService', '$routeParams', function($scope, $location, playerService, $routeParams) {
     if($routeParams.id) {
         playerService.getPlayer($routeParams.id)
             .success(function (result) {
